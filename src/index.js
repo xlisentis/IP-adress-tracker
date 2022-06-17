afa38d0a2ae7026518550a0b3c7094ab772b7d73
Vsevolod Hella <xlisentis@gmail.com>
2022-06-17 18:28:19 +0300

    Added map functionality

--- src/index.js
+++ src/index.js
@@ -5,10 +5,26 @@ const ipInfo = document.querySelector("#ip");
 const locatonInfo = document.querySelector("#location");
 const tmInfo = document.querySelector("#timezone");
 const ispInfo = document.querySelector("#isp");
-
+let marker = null;
 
 ipInput.addEventListener('keydown', handleEnter);
 searchBtn.addEventListener('click', loadData);
+
+const mapArea = document.querySelector('.map');
+const map = L.map(mapArea, {
+    center: [50.45, 30.52],
+    zoom: 16,
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+const mIcon = L.icon({
+    iconUrl: '../images/icon-location.svg',
+    iconSize: [30, 40]
+});
 
 function validateIp(ip) {
     const regexExp = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
@@ -34,8 +50,15 @@ function handleEnter(e) {
 }
 
 function setInfo(data) {
+    const {country, region, city, timezone, lat, lng} = data.location;
     ipInfo.innerText = data.ip;
-    locatonInfo.innerText = `${data.location.country} ${data.location.region}, ${data.location.city}`;
-    tmInfo.innerText = data.location.timezone;
+    locatonInfo.innerText = `${country} ${region}, ${city}`;
+    tmInfo.innerText = timezone;
     ispInfo.innerText = data.isp;
+
+    map.setView([lat, lng]);
+    if (marker !== null) {
+        map.removeLayer(marker);
+    }
+    marker = L.marker([lat, lng], {icon: mIcon}).addTo(map);
 }
